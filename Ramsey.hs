d23e4b3349d1eb4de3e7ab0562f69e7dac9444ab
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2011-06-30 20:22:54 +0200

    ramsey

---
+++ Ramsey.hs
@@ -0,0 +1,56 @@
+-- | find colouring without complete subgraphs
+-- example usage: ./dist/build/Ramsey/Ramsey 3 3 3 64
+-- last number is size of graph,
+-- earlier numbers are sizes of forbidden cliques
+
+{-# language PatternSignatures #-}
+
+import Prelude hiding ( not, and, or )
+import qualified Prelude
+
+import Satchmo.Relation
+import Satchmo.Code
+import Satchmo.Boolean
+import Satchmo.Counting
+
+import qualified Satchmo.Binary as B
+
+import Satchmo.Solver.Minisat
+
+import Data.List (sort)
+import qualified Data.Array as A
+import Control.Monad ( guard, when, forM )
+import System.Environment
+import Data.Ix ( range)
+
+
+main :: IO ()
+main = do
+    argv <- getArgs
+    let ns = map read argv
+        cs = init ns 
+        n = last ns
+    Just ( fs :: [ A.Array (Int,Int) Bool ] ) 
+           <- solve $ ramsey cs n
+    putStrLn $ unlines $ do
+         f <- fs
+         let ((u,l),(o,r)) = A.bounds f
+         x <- [ u .. o ]
+         return $ unwords $ do 
+             y <- [ l .. r ]
+             return $ if f A.! (x,y) then "* " else ". "
+
+fill k cs = replicate (k - length cs) ' ' ++ cs
+
+ramsey (cs :: [Int]) (n :: Int) = do
+    fs <- forM cs $ \ c -> 
+         relation ((1 :: Int,1 :: Int),(n,n))
+    forM [ 1 .. n ] $ \ x -> 
+        forM [ x + 1 .. n ] $ \ y -> 
+            assertM $ exactly 1 $ 
+                for fs $ \ f -> f ! (x,y) 
+    return $ forM fs decode
+    
+for = flip map
+
+assertM this = do x <- this ; assert [x]
