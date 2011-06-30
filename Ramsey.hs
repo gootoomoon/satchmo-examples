7916c0fdddc304c1e465f62ca0aae7f67ccc12f9
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2011-07-01 01:45:39 +0200

    version (seems ok)

--- Ramsey.hs
+++ Ramsey.hs
@@ -17,7 +17,7 @@ import qualified Satchmo.Binary as B
 
 import Satchmo.Solver.Minisat
 
-import Data.List (sort)
+import Data.List (sort, tails)
 import qualified Data.Array as A
 import Control.Monad ( guard, when, forM )
 import System.Environment
@@ -38,7 +38,8 @@ main = do
          x <- [ u .. o ]
          return $ unwords $ do 
              y <- [ l .. r ]
-             return $ if f A.! (x,y) then "* " else ". "
+             return $ if f A.! (x,y) 
+                      then "* " else ". "
 
 fill k cs = replicate (k - length cs) ' ' ++ cs
 
@@ -49,8 +50,18 @@ ramsey (cs :: [Int]) (n :: Int) = do
         forM [ x + 1 .. n ] $ \ y -> 
             assertM $ exactly 1 $ 
                 for fs $ \ f -> f ! (x,y) 
+    forM ( zip cs fs ) $ \ (c,f) -> 
+        forM ( cliques c [1..n] ) $ \ xs ->
+            assert $ map not $ do
+                x : ys <- tails xs ; y <- ys
+                return $ not $ f ! (x,y)
     return $ forM fs decode
     
+cliques 0 _ = return []
+cliques k [] = []
+cliques k (x:xs) = 
+      cliques k xs ++ map (x:) ( cliques (k-1) xs)
+
 for = flip map
 
 assertM this = do x <- this ; assert [x]
